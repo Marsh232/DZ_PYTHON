322aecac5164410695aeadb5f539a1e8e6632faf
Marsh232 <trunov112004@gmail.com>
2022-10-21 23:32:51 +0300

    ЗАДАНИЕ 3

--- FILES/files3.py
+++ FILES/files3.py
@@ -1,6 +1,3 @@
-from unicodedata import name
-
-
 def sort_files(name_file, sort_file):
     file = open(name_file, encoding='utf-8')
     s_file = open(sort_file,'w+', encoding='utf-8')
@@ -11,8 +8,7 @@ def sort_files(name_file, sort_file):
         s_file.writelines(line+'\n')
 
 
-
 if __name__ == '__main__':
-    name_f  = 'plan.txt'
-    sort_f = 'sort plan.txt'
+    name_f = 'plan.txt'
+    sort_f = 'sort_plan.txt'
     sort_files(name_f, sort_f)
