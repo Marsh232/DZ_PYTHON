edc9ce38e3ec3c01ca2d47aed0a421288d2d5e9d
Marsh232 <trunov112004@gmail.com>
2022-10-21 23:32:40 +0300

    ЗАДАНИЕ 1

--- FILES/files1.py
+++ FILES/files1.py
@@ -1,12 +1,16 @@
 def numberic(nf_file, nl_file):
-    f_file = open(nf_file, encoding='utf-8')
-    l_file = open(nl_file, 'w+', encoding='utf-8')
-    n = 1
-    for line in f_file.readlines():
-        l_file.writelines(str(n)+ ' ' + line)
-        n += 1
-    f_file.close()
-    l_file.close()
+    try:
+        f_file = open(nf_file, encoding='utf-8')
+        l_file = open(nl_file, 'w+', encoding='utf-8')
+        n = 1
+        for line in f_file.readlines():
+            l_file.writelines(str(n)+ ' ' + line)
+            n += 1
+        f_file.close()
+        l_file.close()
+    except BaseException:
+        print('ОЙ ПРОИЗОШЛА ОШИБКА!\n'
+              'ВОЗМОЖНО ВЫ УКАЗАЛИ НЕВЕРНОЕ ИМЯ ФАЙЛА')
 
 if __name__ == '__main__':
     file1 = input("Введите имя первого: ")
